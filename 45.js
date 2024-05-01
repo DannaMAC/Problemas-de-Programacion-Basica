d508def4512a13973df287ea56f5d119fe145aac
Danna <salcedomaribel768@gmail.com>
2024-05-01 17:51:11 -0600

    Problema 45: Tenemos el archivo DATOS.DAT con la misma estructura anterior, que esta indexado por el campo ID. Crear un programa que nos permita consultar un registro siempre que queramos.

--- 45.js
+++ 45.js
@@ -1,4 +1,3 @@
-//Autor: Danna Maribel Corral Salcedo 
 //45.- Tenemos el archivo DATOS.DAT con la misma estructura anterior, que esta indexado por el
 //campo ID. Crear un programa que nos permita consultar un registro siempre que queramos.
 
