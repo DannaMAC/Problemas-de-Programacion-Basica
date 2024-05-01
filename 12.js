bfe1b4276beb97d830062d6756ed4e0485b96554
Danna <salcedomaribel768@gmail.com>
2024-05-01 17:08:00 -0600

    Problema 12: Hacer un programa que imprima los números del 1 al 100 y que calcule la suma de todos los números pares por un lado, y por otro, la de todos los impares.

--- 12.js
+++ 12.js
@@ -1,4 +1,3 @@
-//Autor: Danna Maribel Corral Salcedo 
 //12.- Hacer un programa que imprima los números del 1 al 100 y que calcule la suma de todos los
 //números pares por un lado, y por otro, la de todos los impares.
 
