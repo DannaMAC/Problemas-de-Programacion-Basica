1962864567cba4880533be88e971580e21250941
Danna <salcedomaribel768@gmail.com>
2024-05-01 17:44:55 -0600

    Problema 40: Una empresa guarda las ventas realizadas por sus tres representantes a lo largo de doce meses de sus cuatro productos, VENTAS( representante, mes, producto ). Queremos proyectar el total de ventas, TOTAL ( mes, producto ), para lo cual sumamos las ventas de cada producto de cada mes de todos los representantes. Imprimir toda la información.

--- 40.js
+++ 40.js
@@ -1,5 +1,4 @@
-//Autor: Danna Maribel Corral Salcedo
-//Una empresa guarda las ventas realizadas por sus tres representantes a lo largo de doce
+//40.- Una empresa guarda las ventas realizadas por sus tres representantes a lo largo de doce
 //meses de sus cuatro productos, VENTAS( representante, mes, producto ). Queremos proyectar el
 //total de ventas, TOTAL ( mes, producto ), para lo cual sumamos las ventas de cada producto de
 //cada mes de todos los representantes. Imprimir toda la información.
