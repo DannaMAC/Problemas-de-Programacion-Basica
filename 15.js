6d64db1bc2bafbd4f39022eeb5e29184fd858e88
Danna <salcedomaribel768@gmail.com>
2024-05-01 17:12:22 -0600

    Problema 15: Introducir dos números por teclado. Imprimir los números naturales que hay entre ambos números empezando por el más pequeño, contar cuántos hay y cuántos de ellos son pares. Calcular la suma de los impares.

--- 15.js
+++ 15.js
@@ -1,4 +1,3 @@
-//Autor: Danna Maribel Corral Salcedo
 //15.- Introducir dos números por teclado. Imprimir los números naturales que hay entre ambos
 //números empezando por el más pequeño, contar cuántos hay y cuántos de ellos son pares.
 //Calcular la suma de los impares.
