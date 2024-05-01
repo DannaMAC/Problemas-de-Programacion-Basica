5557da53fc82cea13ba4e7291885f83edc70ffbd
Danna <salcedomaribel768@gmail.com>
2024-05-01 17:35:59 -0600

    Problema 32: [200~Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista

--- 32.js
+++ 32.js
@@ -1,4 +1,3 @@
-//Autor: Danna Maribel Corral Salcedo
 //32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista
 //debiendo ir cada uno en una fila distinta.
 
