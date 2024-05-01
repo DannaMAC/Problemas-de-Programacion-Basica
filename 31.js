c64a345df3875bb6c34a142f82c917b429405a3d
Danna <salcedomaribel768@gmail.com>
2024-05-01 17:34:45 -0600

    Problema 31: Hacer un programa que nos permita introducir un número por teclado y sobre él se realicen las siguientes operaciones: comprobar si es primo, hallar su factorial o imprimir su tabla de multiplicar.

--- 31.js
+++ 31.js
@@ -1,4 +1,3 @@
-//Autor: Danna Maribel Corral Salcedo
 //31.- Hacer un programa que nos permita introducir un número por teclado y sobre él se realicen
 //las siguientes operaciones: comprobar si es primo, hallar su factorial o imprimir su tabla de
 //multiplicar.
